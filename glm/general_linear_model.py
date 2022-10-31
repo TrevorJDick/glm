52c66c4a1d20fc7cf5f6ad1464a0b38b9b57efcc
TrevorJDick <thefriendyfew@gmail.com>
2022-10-30 19:43:30 -0600

    added catches and checks related to linear independence with helpful messages, updated notes

--- glm/general_linear_model.py
+++ glm/general_linear_model.py
@@ -29,35 +29,38 @@ class GLM:
     
     
     def fit(self, X, y, sample_weights=None):
+        self.domains = GLM.domains_from_X(X) # used for linear indep checks
         A = GLM.create_design_matrix(self.basis_funcs, X)
         W = GLM.get_sample_weight_matrix(sample_weights, y)
         
-        B = GLM.compute_b_matrix(A, W)
+        try:
+            B = GLM.compute_b_matrix(A, W)
+            self.beta = np.dot(B, y)
         
-        self.beta = np.dot(B, y)
-        
-        ### for diagnotics and stats 
-        # trace(W.M)
-        self.dof = np.trace(
-            np.dot(
-                W,
-                GLM.compute_m_matrix(
-                    GLM.compute_projection_matrix(A, B)
+            ### for diagnotics and stats 
+            # trace(W.M)
+            self.dof = np.trace(
+                np.dot(
+                    W,
+                    GLM.compute_m_matrix(
+                        GLM.compute_projection_matrix(A, B)
+                    )
                 )
             )
-        )
-        self.sigma_sqrd = GLM.compute_sigma_sqrd(
-            y - GLM._func_glm(A, self.beta), 
-            W,
-            self.dof
-        )
-        self.var_beta = GLM.compute_var_beta(self.sigma_sqrd, B)
+            self.sigma_sqrd = GLM.compute_sigma_sqrd(
+                y - GLM._func_glm(A, self.beta), 
+                W,
+                self.dof
+            )
+            self.var_beta = GLM.compute_var_beta(self.sigma_sqrd, B)
+        except Exception as e:
+           self._handle_fit_error(e)
         return self
     
     
     def predict(self, X):
-        ### TODO address trade off here:
-        # must recompute design matrix each predict call
+        ### address trade off here:
+        # must recompute design matrix each predict call vs
         # not saving design matrix during fit reduces memory footprint
         return GLM.func_glm(self.basis_funcs, self.beta, X)
     
@@ -140,7 +143,8 @@ class GLM:
         if np.linalg.det(V) == 0:
             raise ValueError(
                 'NO SOLUTION: det(A^T.W.A)=0\n'
-                'Check design matrix or sample weight matrix!'
+                'Check design matrix for linear independence, '
+                'or sample weight matrix contribution to A^T.W.A!'
             )
         B = np.dot(np.linalg.inv(V), ATW)
         del V, ATW
@@ -175,3 +179,90 @@ class GLM:
     @staticmethod
     def compute_var_beta(sigma_sqrd, B):
         return sigma_sqrd * np.dot(B, B.T)
+    
+    
+    @staticmethod
+    def basis_funcs_slice_indices(basis_funcs):
+        # use if ever need to slice cols of A when basis is multidimensional
+        # prepend the zero length for index algorithm purposes
+        basis_lens = [0] + [len(i) for i in basis_funcs]
+        slice_indices = np.array(
+            [
+                np.cumsum(basis_lens[:-1]), 
+                np.cumsum(basis_lens[1:])
+            ]
+        ).T
+        return slice_indices
+    
+    
+    @staticmethod
+    def domains_from_X(X):
+        domains = np.array([np.min(X, axis=0), np.max(X, axis=0)]).T
+        return domains
+    
+    
+    @staticmethod
+    def check_linear_independence_basis_funcs(basis_funcs, domains):
+        step = 0.01
+        lin_indep_pass = []
+        ranks = []
+        for bf, domain in zip(basis_funcs, domains):
+            m = int((domain[1] - domain[0]) / step)
+            n = len(bf)
+            r = np.linalg.matrix_rank(
+                GLM.create_design_matrix(
+                    [bf],
+                    np.linspace(*domain, m).reshape(-1, 1)
+                )
+            )
+            ranks.append(r)
+            if r == n:
+                lin_indep_pass.append(True)
+            else:
+                lin_indep_pass.append(False)
+        return lin_indep_pass, ranks
+    
+    
+    @staticmethod
+    def linear_independence_check_print(basis_funcs, lin_indep_pass, ranks):
+        for i, (bf, p, r) in enumerate(zip(basis_funcs, lin_indep_pass, ranks)):
+            basis_message = 'TRUE -- basis function are linearly independent'
+            if not p:
+                basis_message =  'FALSE -- basis function are NOT linearly independent!'
+            print(
+                f'{i + 1} -- coordinate dimension:\n'
+                f'{basis_message}\n'
+                f'{r} -- rank of design matrix\n'
+                f'{len(bf)} -- expected rank.\n'
+            )
+        print(
+            'NOTE: once linear independance is re-established it is best to also '
+            'check the data in X, y is reasonably filling up the space it is '
+            'contained in.\n'
+        )
+        return
+    
+    
+    def _handle_fit_error(self, exception):
+        print(exception)
+        lin_indep_pass, ranks = GLM.check_linear_independence_basis_funcs(
+            self.basis_funcs,
+            self.domains
+        )
+        if all(lin_indep_pass):
+            print(
+                'All basis functions, for each dimension, are linearly '
+                'independent.  The failure of finding an inverse for '
+                'A^T.W.A is likely due to the behavoir of the higher '
+                'dimensional data in X, y actually behaving as lower '
+                'dimensional data.  An example is a 1D line embedded in 3D. '
+                'The alternate possibility is the nature of sample weights, '
+                'if they were provided.'
+            )
+        else:
+            GLM.linear_independence_check_print(
+                self.basis_funcs,
+                lin_indep_pass,
+                ranks
+            )
+        return
